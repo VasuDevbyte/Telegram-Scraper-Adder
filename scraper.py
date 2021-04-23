b79d8dfe1fae0a22e710bc116523b57576eb18da
VasuDevbyte <81460710+VasuDevbyte@users.noreply.github.com>
2021-04-23 13:54:44 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,8 +17,8 @@ def banner():
 {re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
 
               Version : 1.01
- {re}Subscribe Termux Professor on Youtube.
-   {cy}www.youtube.com/c/TermuxProfessorYT
+ {re}Thanks For Using It!.
+   {cy}https://t.me/Mrvasuz
         """)
 
 cpass = configparser.RawConfigParser()
